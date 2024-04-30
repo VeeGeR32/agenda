cf101e8d479f6f53ff144dff0956787662c31c50
VeeGeR <90403166+VeeGeR32@users.noreply.github.com>
2024-04-30 16:12:40 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Home";
-import AboutPage from "./pages/About";
-import Application from "./pages/Application";
-import Account from "./pages/Account";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Todo from "./App/TODO/Todo";
-// Importez d'autres pages ici
+import React from 'react';
+import addNotification from 'react-push-notification';
+import { Notifications } from 'react-push-notification';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/application" element={<Application />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/todo" element={<Todo />} />
+  function buttonOnClick() {
+    addNotification({
+      title: 'Warning',
+      subtitle: 'This is a subtitle',
+      message: 'This is a message',
+      theme: 'darkblue',
+      native: true
+    });
+  }
 
-        </Routes>
-    </BrowserRouter>
+  return (
+    <div className="App flex justify-center items-center h-screen bg-gray-100">
+      <div>
+        <Notifications />
+        <button
+          onClick={buttonOnClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Push Notification
+        </button>
+      </div>
+    </div>
   );
 }
 
